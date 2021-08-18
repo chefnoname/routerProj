bfdfb5e12beb896a1be0287dc6249fb42453daea
Chef NoName <abdijama21@gmail.com>
2021-08-18 02:46:03 +0100

    update

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,17 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: `Abu Mus'ab ash-Shaafi'i`,
+    text: "Get a load of this guy...",
+  },
+  { id: "q2", author: `Ahmed an-Nahwi`, text: "Nigashiima! Nigashiima!" },
+  { id: "q3", author: `King Soloman K`, text: "Habibi." },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,13 +1,30 @@
 import { Route, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: `Abu Mus'ab ash-Shaafi'i`,
+    text: "Get a load of this guy...",
+  },
+  { id: "q2", author: `Ahmed an-Nahwi`, text: "Nigashiima! Nigashiima!" },
+  { id: "q3", author: `King Soloman K`, text: "Habibi." },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  console.log(params.quoteId);
+
+  if (!quote) {
+    return <p>No Quote Found</p>;
+  }
+
   return (
     <>
-      <h1> Quotes Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
